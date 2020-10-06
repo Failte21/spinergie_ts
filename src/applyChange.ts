70a76aeb1d424a7aed7cd0adb8a1e14dd37eda8d
leo <lsimon@greenflex.com>
2020-10-06 17:34:20 +0200

    applychange: without ramda

--- src/applyChange.ts
+++ src/applyChange.ts
@@ -1,11 +1,21 @@
-import { lensPath, set } from "ramda";
+type Change = { path: [string]; value: String };
 
-type Change = { path: [String]; value: String };
+const set = (path: [string] | [], value: unknown, object: Object) => {
+  const [first, ...rest] = path;
+
+  if (rest.length == 0) {
+    return { ...object, [first]: value };
+  }
+  if (object[first]) {
+    return { ...object, [first]: set(rest, value, object[first]) };
+  }
+  return object;
+};
 
 const applyChange = (original: Object, changes: [Change]) => {
   return changes.reduce((acc, { path, value }) => {
-    const lens = lensPath(path);
-    return set(lens, value, acc);
+    // const lens = lensPath(path);
+    return set(path, value, acc);
   }, original);
 };
 
